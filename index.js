4718de7174e68db34bead76026d38f35e196c375
rodrigocarajas24-droid <rodrigo.carajas24@gmail.com>
2026-07-24 00:39:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        executablePath: '/opt/render/.cache/puppeteer/chrome/linux-146.0.7680.31/chrome-linux64/chrome',
+        executablePath: '/usr/bin/google-chrome-stable',
         headless: true,
         args: [
             '--no-sandbox',
