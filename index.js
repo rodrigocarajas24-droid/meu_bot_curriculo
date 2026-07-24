539ec9400fb8dc3578e09266e3efdeb53a71e54a
rodrigocarajas24-droid <rodrigo.carajas24@gmail.com>
2026-07-23 23:06:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ const path = require('path');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        executablePath: require('puppeteer').executablePath(),
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
